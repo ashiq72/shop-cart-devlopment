4c326f252b49ee49e499d7ae29a2852edf3b516a
Ashik Ahmed <ahmedashik18k@gmail.com>
2022-08-21 06:53:15 +0600

    Only design upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsCenter = document.querySelector(".products-center");
+
+// Cart
+let cart = [];
+
+//getting the products
+class Products {}
+
+//display products
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+   const ui = new UI();
+   const products = new Products();
+});
