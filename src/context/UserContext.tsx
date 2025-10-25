2a0786e1170bd67dc46d1598a0b40302c896a661
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-10-25 20:30:09 +0600

    useContext added for  findout user deatils in cooikes

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,10 +1,25 @@
 import { getCurrentUser } from "@/services/AuthService";
-import { createContext, useEffect, useState } from "react";
+import { IUser } from "@/types";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-const UserContext = createContext(undefined);
+interface IUserProviderValues {
+  user: IUser | null;
+  isLoading: boolean;
+  setUser: (user: IUser | null) => void;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+const UserContext = createContext<IUserProviderValues | undefined>(undefined);
 
 const UserProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<IUser | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const handleUser = async () => {
@@ -23,3 +38,15 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
     </UserContext.Provider>
   );
 };
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context == undefined) {
+    throw new Error("useUser must be used within the UserProvider context");
+  }
+
+  return context;
+};
+
+export default UserProvider;
